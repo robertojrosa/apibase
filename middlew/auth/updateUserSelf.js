41ee402dcec8fbee08eaba97add5ee03ad59643f
rjrosa <rjrosa@gmail.com>
2021-05-30 09:50:35 -0600

    moving day

--- middlew/auth/updateUserSelf.js
+++ middlew/auth/updateUserSelf.js
@@ -1,10 +1,14 @@
 const User = require("../../models/User");
 
 module.exports = async (req, res, next) => {
+  try{
   var existingUser = await User.findOne({where:{id : req.body.userIdFromCookie}})
   existingUser.firstName=req.body.firstName,
   existingUser.lastName=req.body.lastName,
   existingUser.email=req.body.email
   await existingUser.save()
   next();
+  } catch(err) {
+    res.status(400).json({message: 'unable to update'})
+  }
 };
--- middlew/auth/validateCookie.js
+++ middlew/auth/validateCookie.js
@@ -2,13 +2,13 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   const { cookies } = req;
   if (typeof cookies.access_token === "undefined" || !cookies.access_token) {
-    res.status(401).redirect("login/");
+    res.status(401).json({message: 'please login'});
     return;
   } else var token = cookies.access_token.split(" ");
 
   jwt.verify(token[1], process.env.JWT_SECRET, function (err, decoded) {
     if (err) {
-      res.status(401).redirect("login/");
+      res.status(401).json({message: 'please login'});
       return;
     } else {
       req.body.userIdFromCookie = decoded.id
